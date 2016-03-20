e469ec61f9970f3c0011fb8a4e68a5d750758f75
Andrew <awithey@gmail.com>
2016-03-20 21:44:21 +0000

    Add batchsize

--- neo2csv.py
+++ neo2csv.py
@@ -3,28 +3,13 @@ from py2neo import authenticate, Graph, Node, Relationship
 import sys, getopt, os
 import csv, codecs, cStringIO
 
-class Neo4jConfig(object):
-    def __init__(self):
-        # Defaults
-        self.protocol = "http"
-        self.hostName = "localhost"
-        self.portNumber = 7474
-        self.dbPath = "/db/data/"
-        self.userId = ""
-        self.password = ""
-        self.limit = 0 # limit = 0 means no limit, limit =n where n > 0 limits queries to at most n rows
-        self.csvpath = "."
-        self.nodeLabels = ""
+from py2neo.packages.httpstream import http
 
-    def host(self):
-        return self.hostName + ":" + str(self.portNumber)
+http.socket_timeout = 9999
 
-    def url(self):
-        return self.protocol + "://" + self.host() + self.dbPath
 
 # This is from http://stackoverflow.com/questions/5838605/python-dictwriter-writing-utf-8-encoded-csv-files :-)
 class DictUnicodeWriter(object):
-
     def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
         # Redirect output to a queue
         self.queue = cStringIO.StringIO()
@@ -51,6 +36,7 @@ class DictUnicodeWriter(object):
     def writeheader(self):
         self.writer.writeheader()
 
+
 class table(object):
     def __init__(self):
         self.columnHeadings = []
@@ -59,19 +45,20 @@ class table(object):
     def addRow(self, row):
         for col_name in row:
             if col_name not in self.columnHeadings:
-                self.columnHeadings.append(col_name) # accumulate column headings for later output
+                self.columnHeadings.append(col_name)  # accumulate column headings for later output
         self.rows.append(row)
 
     def saveCsv(self, path, filename):
-        fieldnames=[s.encode("utf-8") for s in self.columnHeadings]
+        fieldnames = [s.encode("utf-8") for s in self.columnHeadings]
         fieldnames.sort()
-        fullpath = os.path.join(path,filename)
+        fullpath = os.path.join(path, filename)
         with open(fullpath, 'wb') as csvfile:
             print "\tWriting to", fullpath
-            csvfile.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+            csvfile.write(u'\ufeff'.encode('utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
             csvwriter = DictUnicodeWriter(csvfile, fieldnames)
             csvwriter.writeheader()
             csvwriter.writerows(self.rows)
+
 
 def usage():
     print "Usage", sys.argv[0]
@@ -86,115 +73,147 @@ def usage():
     print " csvpath=[path to folder where CSV files are written] {default=.}"
     print " nodelabels=[comma delimited list of node labels, get list from db if empty] {default=}"
 
+
+
+
 def main():
+    # Defaults
+    protocol = "http"
+    hostName = "localhost"
+    portNumber = 7474
+    dbPath = "/db/data/"
+    userId = ""
+    password = ""
+    limit = 0  # limit = 0 means no limit, limit =n where n > 0 limits queries to at most n rows
+    csvpath = "."
+    nodeLabels = ""
+    batchsize = 1000
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"?",["help","protocol=","host=","port=","db=","userid=","password=","limit=","csvpath=","nodelabels="])
+        opts, args = getopt.getopt(sys.argv[1:], "?",
+                                   ["help", "protocol=", "host=", "port=", "db=", "userid=", "password=", "limit=",
+                                    "csvpath=", "nodelabels="])
     except getopt.GetoptError:
         print "ERROR unknown option!"
         print ""
         usage()
         sys.exit(2)
 
-    config = Neo4jConfig()
-
     for opt, arg in opts:
         if opt in ('-?', '--help'):
             usage()
             sys.exit(0)
         elif opt == "--protocol":
-            config.protocol = arg
+            protocol = arg
         elif opt == "--host":
-            config.hostName = arg
+            hostName = arg
         elif opt == "--port":
-            config.portNumber = arg
+            portNumber = arg
         elif opt == "--db":
-            config.dbPath = arg
+            dbPath = arg
         elif opt == "--userid":
-            config.userId = arg
+            userId = arg
         elif opt == "--password":
-            config.password = arg
+            password = arg
         elif opt == "--limit":
-            config.limit = arg
+            limit = arg
         elif opt == "--csvpath":
-            config.csvpath = arg
+            csvpath = arg
         elif opt == "--nodelabels":
-            config.nodeLabels = arg
+            nodeLabels = arg
         else:
             print "ERROR: Unknown option", opt
             print ""
             usage()
             sys.exit(2)
 
-    print "Connect to", config.url()
+    host = hostName + ":" + str(portNumber)
+    url = protocol + "://" + host + dbPath
 
-    if len(config.userId) > 0:
-        authenticate(config.host(), config.userId, config.password)
+    print "Connect to", url
 
-    graph = Graph(config.url())
+    if len(userId) > 0:
+        authenticate(host, userId, password)
+
+    graph = Graph(url)
     print "Connected:", graph.bound
 
-    #Check if output directory exists
-    if not os.path.isdir(config.csvpath):
-        print "ERROR Directory doesn't exist", config.csvpath
+    # Check if output directory exists
+    if not os.path.isdir(csvpath):
+        print "ERROR Directory doesn't exist", csvpath
         sys.exit(1)
 
-    relationshipTables = {} # one table for each of the relationship types for the current label
+    relationshipTables = {}  # one table for each of the relationship types for the current label
 
-    if len(config.nodeLabels) > 0:
-        labels = config.nodeLabels.split(',')
+    if len(nodeLabels) > 0:
+        labels = nodeLabels.split(',')
     else:
-        labels = graph.node_labels # Get list of labels from the database
+        labels = graph.node_labels  # Get list of labels from the database
 
     for label in labels:
         print "Get nodes for label:", label
 
         currentTable = table()
 
-        if config.limit > 0:
-            print "*** Get Nodes with limit", config.limit
-            nodes = graph.find(label, limit=config.limit)
-        else:
-            nodes = graph.find(label)
+        nodecount=0
 
-        for node in nodes:
-            row = node.properties.copy()
-            row["nodeId"] = node._id
-            currentTable.addRow(row)
-
-            # get relationships
-            if config.limit > 0:
-                # limit is for n relationships per node, so you may end up with n^2 relationships!
-                nodeRelationships = graph.match(start_node=node, limit=config.limit)
-            else:
-                nodeRelationships = graph.match(start_node=node)
-
-
-            for rel in nodeRelationships:
-                relTableName = str(label + "_" + rel.type)
-
-                if relTableName in relationshipTables:
-                    relTable = relationshipTables[relTableName]
+        while True:
+            matchLimit = batchsize
+            if limit > 0:
+                if nodecount < limit:
+                    if nodecount + batchsize > limit:
+                        matchLimit = limit - nodecount
                 else:
-                    relTable = table()
-                    relationshipTables[relTableName]=relTable
+                    break
 
-                relRows = rel.properties.copy()
-                relRows["nodeId"] = node._id
-                relRows["otherNodeId"] = rel.end_node._id
+            # nodes = graph.find(label)
+            nodes = graph.cypher.stream("MATCH (n:" + str(label) + ") RETURN n ORDER BY id(n) SKIP {skip} LIMIT {limit}", {"skip": nodecount, "limit": matchLimit})
 
-                relTable.addRow(relRows)
+            if not nodes:
+                break
+
+            for g_node in nodes:
+                nodecount += 1
+                node = g_node.n
+                row = node.properties.copy()
+                row["nodeId"] = node._id
+                currentTable.addRow(row)
+
+                # get relationships
+                if limit > 0:
+                    # limit is for n relationships per node, so you may end up with n^2 relationships!
+                    nodeRelationships = graph.match(start_node=node, limit=limit)
+                else:
+                    nodeRelationships = graph.match(start_node=node)
+
+                for rel in nodeRelationships:
+                    relTableName = str(label + "_" + rel.type)
+
+                    if relTableName in relationshipTables:
+                        relTable = relationshipTables[relTableName]
+                    else:
+                        relTable = table()
+                        relationshipTables[relTableName] = relTable
+
+                    relRows = rel.properties.copy()
+                    relRows["nodeId"] = node._id
+                    relRows["otherNodeId"] = rel.end_node._id
+
+                    relTable.addRow(relRows)
+            print "\tNode count", nodecount
 
         tableCsvFileName = label + ".csv"
         print "Export label CSV", tableCsvFileName
-        currentTable.saveCsv(config.csvpath,tableCsvFileName)
+        currentTable.saveCsv(csvpath, tableCsvFileName)
 
     for relTableName in relationshipTables:
         relTable = relationshipTables[relTableName]
         relTableCsvFileName = relTableName + ".csv"
         print "Export relationship CSV", relTableCsvFileName
-        relTable.saveCsv(config.csvpath,relTableCsvFileName)
+        relTable.saveCsv(csvpath, relTableCsvFileName)
 
     print "Finished"
+
 
 if __name__ == "__main__":
     main()
